234fe617ace3a9b9ccff0b762def7afef9ce3b2c
Paul Wilson <pwilson802@gmail.com>
2021-01-20 21:37:28 +1100

    updated twitter card

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,26 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:description"
+          content="Not sure what to watch? CouchBuddy's got you sorted"
+        />
+        <meta name="twitter:title" content="CouchBuddy" />
+        <meta name="twitter:site" content="@couch_buddy" />
+        <meta
+          name="twitter:image"
+          content="https://couchbuddy.s3-ap-southeast-2.amazonaws.com/twitterCard.png"
+        />
+        <meta property="og:title" content="CouchBuddy" />
+        <meta
+          property="og:description"
+          content="Not sure what to watch? CouchBuddy's got you sorted"
+        />
+        <meta
+          property="og:image"
+          content="https://couchbuddy.s3-ap-southeast-2.amazonaws.com/twitterCard.png"
+        />
       </Head>
 
       <main>
