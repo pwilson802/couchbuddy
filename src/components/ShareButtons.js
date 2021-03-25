12c8f0dd279a8434aaf7059f984c54159bad6ed4
Paul Wilson <pwilson802@gmail.com>
2021-03-25 23:09:22 +1100

    updated twitter share

--- src/components/ShareButtons.js
+++ src/components/ShareButtons.js
@@ -19,20 +19,40 @@ function ShareButtons({ movie, tagline }) {
     shareButton: css({
       marginRight: 15,
     }),
+    image: css({
+      width: 32,
+      height: 32,
+    }),
   };
   const shareMessage = `I'm watching ${movie}.\r\n\r\n${tagline}`;
-  const twitterShareMessage = `I'm watching ${movie}.  @couch_buddy \r\n\r\n${tagline}`;
+  const facebookShareMessage = `I'm watching ${movie}.\n${tagline}`;
+  const twitterShareMessage = `I'm watching ${movie}.\n${tagline}\n\n@couch_buddy\n`;
+  // const twitterIntent = "https://twitter.com/intent/tweet";
+  // const url = "https://couchbuddy.info";
+  // const shareURLTwitter = new URL(
+  //   `${twitterIntent}?url=${url}&text=${twitterShareMessage}`
+  // );
+  const shareURLTwitter =
+    "https://twitter.com/intent/tweet?url=https://couchbuddy.info&text=" +
+    encodeURIComponent(twitterShareMessage);
+  const shareURLFacebook =
+    "https://www.facebook.com/sharer/sharer.php?u=https://couchbuddy.info&quote=" +
+    encodeURIComponent(facebookShareMessage);
   return (
     <div css={styles.shareButtons}>
       <div css={styles.shareButton}>
-        <TwitterShareButton
-          url={"https://couchbuddy.info"}
-          title={twitterShareMessage}
-        >
-          <TwitterIcon size={32} round={true} />
-        </TwitterShareButton>
+        <a href={shareURLTwitter} target="_blank">
+          <img css={styles.image} src="/share/twitter.png" alt="twitter logo" />
+        </a>
       </div>
       <div css={styles.shareButton}>
+        {/* <a href={shareURLFacebook} target="_blank">
+          <img
+            css={styles.image}
+            src="/share/facebook.png"
+            alt="facebook logo"
+          />
+        </a> */}
         <FacebookShareButton
           url={"https://couchbuddy.info"}
           quote={shareMessage}
