6e20efed861da4390f5738b1f7cd3ebda0b51cda
Paul Wilson <pwilson802@gmail.com>
2021-04-26 22:28:20 +1000

    removed console.log

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -25,7 +25,6 @@ function MyApp({ Component, pageProps }) {
   const updateConsent = (con) => {
     localStorage.setItem("consent", con);
     if (con == "yes") {
-      console.log("consented yes");
       window["ga-disable-G-HE4FSJS60K"] = false;
       window.dataLayer = window.dataLayer || [];
       function gtag() {
