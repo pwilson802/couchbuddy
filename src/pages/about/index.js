461361927ee41f0a479c486fb0e5c9f30240e54f
Paul Wilson <pwilson802@gmail.com>
2022-03-28 22:32:05 +1100

    reverted the pictures

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -124,7 +124,7 @@ function About({ setConsent, location, handleLocation }) {
         <meta name="twitter:site" content="@couch_buddy" />
         <meta
           name="twitter:image"
-          content="https://d1ohygkqc6tgyk.cloudfront.net/twitter-card-main5.png"
+          content="https://couchbuddy-images.s3.amazonaws.com/twitter-card-main.png"
         />
         <meta property="og:title" content="CouchBuddy" />
         <meta
@@ -133,7 +133,7 @@ function About({ setConsent, location, handleLocation }) {
         />
         <meta
           property="og:image"
-          content="https://d1ohygkqc6tgyk.cloudfront.net/twitter-card-main5.png"
+          content="https://couchbuddy-images.s3.amazonaws.com/twitter-card-main.png"
         />
       </Head>
       <main css={styles.aboutWrapper}>
