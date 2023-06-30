2f809c3facd2f0885b06dc03afacacaf42f229f9
Paul Wilson <paul.wilson@unifii.com.au>
2023-06-30 21:14:40 +1000

    updated sentry config

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -7,7 +7,7 @@ import * as Sentry from '@sentry/nextjs';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://54ce81e7d11946339ed58c8f400a5ef1@o1165529.ingest.sentry.io/6255675',
+  dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
